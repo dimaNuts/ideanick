7d47f8b328496ba0c9ed10ebcde95dd20c359eb3
nuts <nuts272@mail.ru>
2024-11-29 14:52:38 +0300

    Change

---
+++ webapp/src/App.tsx
@@ -0,0 +1,31 @@
+export const App = () => {
+	const ideas = [
+		{ nick: "cool-nick-idea-1", name: "Idea1", descriotion: "Idea1 description..." },
+		{ nick: "cool-nick-idea-2", name: "Idea2", descriotion: "Idea2 description..." },
+		{ nick: "cool-nick-idea-3", name: "Idea3", descriotion: "Idea3 description..." },
+		{ nick: "cool-nick-idea-4", name: "Idea4", descriotion: "Idea4 description..." },
+		{ nick: "cool-nick-idea-5", name: "Idea5", descriotion: "Idea5 description..." },
+	];
+
+
+	return (
+
+		<div>
+			<h1>Ideanick</h1>
+			{ideas.map((idea) => {
+				return (
+					<div key={idea.nick}>
+						<h2>{idea.name}</h2>
+						<p>{idea.descriotion}</p>
+					</div>
+
+				);
+			})
+
+			}
+		</div>
+
+
+	)
+};
+
