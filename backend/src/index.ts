db1093f9dfb5e1995b05e3571f3b82d3244bac91
nuts <nuts272@mail.ru>
2024-11-30 20:20:35 +0300

    * Create trpc.ts * Add ideas[] in trpc.ts * Remove ideas[] from index.ts * Add router in trpc.ts * Add midleware in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,7 @@
 import express from 'express';
+import { trpcRouter } from './trpc';
+import * as trpcExpress from '@trpc/server/adapters/express';
 
-const ideas = [
-	{ nick: "cool-nick-idea-1", name: "Idea1", descriotion: "Idea1 description..." },
-	{ nick: "cool-nick-idea-2", name: "Idea2", descriotion: "Idea2 description..." },
-	{ nick: "cool-nick-idea-3", name: "Idea3", descriotion: "Idea3 description..." },
-	{ nick: "cool-nick-idea-4", name: "Idea4", descriotion: "Idea4 description..." },
-	{ nick: "cool-nick-idea-5", name: "Idea5", descriotion: "Idea5 description..." },
-];
 
 const expressApp = express();
 
@@ -14,9 +9,12 @@ expressApp.get('/ping', (req, res) => {
 	res.send('pong');
 });
 
-expressApp.get('/ideas', (req, res) => {
-	res.send(ideas);
-})
+expressApp.use(
+	'/trpc',
+	trpcExpress.createExpressMiddleware({
+		router: trpcRouter,
+	})
+)
 
 expressApp.listen(3000, () => {
 	console.info('Listening at http://localhost:3000');
---
+++ backend/src/trpc.ts
@@ -0,0 +1,18 @@
+import { initTRPC } from '@trpc/server';
+
+
+const ideas = [
+	{ nick: "cool-nick-idea-1", name: "Idea1", descriotion: "Idea1 description..." },
+	{ nick: "cool-nick-idea-2", name: "Idea2", descriotion: "Idea2 description..." },
+	{ nick: "cool-nick-idea-3", name: "Idea3", descriotion: "Idea3 description..." },
+	{ nick: "cool-nick-idea-4", name: "Idea4", descriotion: "Idea4 description..." },
+	{ nick: "cool-nick-idea-5", name: "Idea5", descriotion: "Idea5 description..." },
+];
+
+const trpc = initTRPC.create();
+
+export const trpcRouter = trpc.router({
+	getIdeas: trpc.procedure.query(() => {
+		return { ideas }
+	}),
+});
