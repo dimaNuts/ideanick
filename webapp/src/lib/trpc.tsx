70786b243e3826d87cf0619f1900adf0737c5579
nuts <nuts272@mail.ru>
2024-12-03 09:58:27 +0300

    Set trpc at front/Create dir lib/Add file trpc for front

---
+++ webapp/src/lib/trpc.tsx
@@ -0,0 +1,32 @@
+import { createTRPCReact, httpBatchLink } from "@trpc/react-query";
+import type { TrpcRouter } from "@fullstackstepik/backend/src/trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const trpc = createTRPCReact<TrpcRouter>();
+
+// клиент react-query документация: https://tanstack.com/query/latest
+// управляет логикой запросов(не типами) 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
+// trpcClient должен знать где endpoint для всех роутеров
+const trpcClient = trpc.createClient({
+	links: [
+		httpBatchLink({
+			url: 'http://localhost:3000/trpc',
+		}),
+	],
+});
+
+export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
+	return (<trpc.Provider client={trpcClient} queryClient={queryClient}>
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+	</trpc.Provider>);
+}
