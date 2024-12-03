842cf4448f48bea741f837d4cb0a55fb08b887ca
nuts <nuts272@mail.ru>
2024-12-03 10:27:07 +0300

    * Change App.tsx in front * Rename App -> AllIdeasPage * Add App -> add into TrpcProvider AllIdeasPage * Create dir pages * Move AllIdeasPage from App.tsx in newfile index.tsx dir AllIdeasPage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,11 @@
+import { TrpcProvider } from './lib/trpc';
+import { AllIdeasPage } from './pages/AllIdeasPage/index';
+
+
+// передаем все страницы внутрь TrpcProvider,
+// благодаря этому внутри страничек будут доступны Trpc-функции
 export const App = () => {
-	const ideas = [
-		{ nick: "cool-nick-idea-1", name: "Idea1", descriotion: "Idea1 description..." },
-		{ nick: "cool-nick-idea-2", name: "Idea2", descriotion: "Idea2 description..." },
-		{ nick: "cool-nick-idea-3", name: "Idea3", descriotion: "Idea3 description..." },
-		{ nick: "cool-nick-idea-4", name: "Idea4", descriotion: "Idea4 description..." },
-		{ nick: "cool-nick-idea-5", name: "Idea5", descriotion: "Idea5 description..." },
-	];
-
-
 	return (
-
-		<div>
-			<h1>Ideanick</h1>
-			{ideas.map((idea) => {
-				return (
-					<div key={idea.nick}>
-						<h2>{idea.name}</h2>
-						<p>{idea.descriotion}</p>
-					</div>
-
-				);
-			})
-
-			}
-		</div>
-
-
-	)
-};
-
+		<TrpcProvider><AllIdeasPage /></TrpcProvider>
+	);
+}
