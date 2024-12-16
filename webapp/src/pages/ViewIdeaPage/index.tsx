aff2046d73a450d05376f65d32de118c197c9ac1
nuts <nuts272@mail.ru>
2024-12-16 17:39:42 +0300

    Create folder ViewIdeaPage/Create Idea 1

---
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -0,0 +1,13 @@
+export const ViewIdeaPage = () => {
+  return (
+    <div>
+      <h1>Idea 1</h1>
+      <p>Descriotion of idea 1...</p>
+      <div>
+        <p>Tex paragrah 1 of idea 1 ...</p>
+        <p>Tex paragrah 2 of idea 1 ...</p>
+        <p>Tex paragrah 3 of idea 1 ...</p>
+      </div>
+    </div>
+  )
+}
