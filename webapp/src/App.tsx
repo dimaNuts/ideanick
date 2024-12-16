0862cfde1c14ba7575521b7e6d2ea773d7d1d7c8
nuts <nuts272@mail.ru>
2024-12-16 17:54:40 +0300

    Install react-router-dom, react-dom in webapp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage/index'
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
 
 // передаем все страницы внутрь TrpcProvider,
 // благодаря этому внутри страничек будут доступны Trpc-функции
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <AllIdeasPage />
+          <ViewIdeaPage />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
