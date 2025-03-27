034979f829d1f785ec504c8e25e41aadd4597651
nuts <nuts272@mail.ru>
2025-03-27 18:13:45 +0300

    add comment

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -1,5 +1,11 @@
 // файл содержит роутеры для перехода по стр.
 
+// дженерик создаем { ideaNick: ':ideaNick' } и {ideaNick: string}
+// в getRouteParams() передаём объект с ключом ideaNick, затем исп. reduce()
+// в 'acc' попадет пустой массив, а в 'key' ideaNick(как ключ полученный путем исп. Object.keys(object))
+// getRouteParams() возвращает тип Record(объект), где ключ это ключ 'T' со значением string
+// в то же время 'T' наследует тип  Record<string, boolean>, т.е.
+// тот тип, который передаем затем getRouteParams({ ideaNick: true })
 const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
   return Object.keys(object).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
 }
