09dc0c54868ccb2b04fb7eedb3f2b3e7bc1dd7a4
nuts <nuts272@mail.ru>
2025-04-11 10:41:23 +0300

    out validation

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -1,21 +1,10 @@
-import { z } from 'zod'
+//
 import { ideas } from '../../lib/ideas'
 import { trpc } from '../../lib/trpc'
+import { zCreateIdeaTrpcInput } from './input'
 
 // получение идеи с бэка
-export const createIdeaTrpcRoute = trpc.procedure
-  .input(
-    z.object({
-      name: z.string().min(1),
-      nick: z
-        .string()
-        .min(1)
-        .regex(/^[a-z0-9-]+$/, 'Nick contain only lowercase letters, number and dashes'),
-      description: z.string().min(1),
-      text: z.string().min(100, 'Text should be at least 100 characters long'),
-    })
-  )
-  .mutation(({ input }) => {
-    ideas.unshift(input)
-    return true
-  })
+export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(({ input }) => {
+  ideas.unshift(input)
+  return true
+})
--- backend/src/router/createIdea/input.ts
+++ backend/src/router/createIdea/input.ts
@@ -1,1 +1,11 @@
-// test
+import { z } from 'zod'
+
+export const zCreateIdeaTrpcInput = z.object({
+  name: z.string().min(1),
+  nick: z
+    .string()
+    .min(1)
+    .regex(/^[a-z0-9-]+$/, 'Nick contain only lowercase letters, number and dashes'),
+  description: z.string().min(1),
+  text: z.string().min(100, 'Text should be at least 100 characters long'),
+})
