ccdd8da85eb46690f609f2b4caa107c969869169
nuts <nuts272@mail.ru>
2025-04-11 14:09:13 +0300

    add styles in textarea form

--- webapp/src/components/Textare/index.tsx
+++ webapp/src/components/Textare/index.tsx
@@ -1,14 +1,23 @@
+import cn from 'classnames'
 import { type FormikProps } from 'formik'
+import css from './index.module.scss'
 
 export const Textarea = ({ label, name, formik }: { label: string; name: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
   const error = formik.errors[name] as string | undefined
   const touched = formik.touched[name]
+  const disabled = formik.isSubmitting
+  const invalid = !!touched && !!error
   return (
-    <div style={{ marginBottom: 10 }}>
-      <label htmlFor={name}>{label}</label>
-      <br />
+    <div className={cn({ [css.field]: true, [css.disabled]: disabled })}>
+      <label className={css.label} htmlFor={name}>
+        {label}
+      </label>
       <textarea
+        className={cn({
+          [css.input]: true,
+          [css.invalid]: invalid,
+        })}
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
@@ -18,9 +27,9 @@ export const Textarea = ({ label, name, formik }: { label: string; name: string;
         value={value}
         name={name}
         id={name}
-        disabled={formik.isSubmitting}
+        disabled={disabled}
       />
-      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
+      {invalid && <div className={css.error}>{error}</div>}
     </div>
   )
 }
