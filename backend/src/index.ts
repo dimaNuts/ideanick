3d973a567316797db6712cc0407a4d5fa72ed48e
nuts <nuts272@mail.ru>
2025-05-01 13:48:11 +0300

    intit context --> called createAppContext in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,27 @@
 import cors from 'cors'
 import express from 'express'
+import { type AppContext, createAppContext } from './lib/ctx'
 import { applyTrpcToExpressApp } from './lib/trpc'
 import { trpcRouter } from './router'
 
-const expressApp = express()
-expressApp.use(cors())
+void (async () => {
+  let ctx: AppContext | null = null
+  try {
+    ctx = createAppContext()
+    const expressApp = express()
+    expressApp.use(cors())
 
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
+    expressApp.get('/ping', (req, res) => {
+      res.send('pong')
+    })
 
-applyTrpcToExpressApp(expressApp, trpcRouter)
+    applyTrpcToExpressApp(expressApp, trpcRouter)
 
-expressApp.listen(3000, () => {
-  console.info('Listening at http://localhost:3000')
-})
+    expressApp.listen(3000, () => {
+      console.info('Listening at http://localhost:3000')
+    })
+  } catch (error) {
+    console.error(error)
+    await ctx?.stop()
+  }
+})()
